684c45fc83dab0f244aa3ba25f5c98df68d04ec2
Anton von Weltzien <a.weltzien@outlook.de>
2019-07-01 14:08:56 +0200

    Refactor

--- replication_python.py
+++ replication_python.py
@@ -1,53 +1,11 @@
 import copy
 
 from pulp import *
-import random
-import numpy as np
 from anytree import PreOrderIter, RenderTree, DoubleStyle
 import itertools
 from anytree import Node as AbstractNode
 
-
-def generate_queries(param_num_queries, param_num_fragments):
-    queries = []
-    for i in range(param_num_queries):
-        temp_list = []
-        for j in range(param_num_fragments):
-            temp_list.append(random.choice([0, 1]))
-        queries.append(temp_list)
-    return queries
-
-
-def print_location(var, index_x, index_y, cast_to_int=True):
-    for n in range(index_x):
-        print_string = "Node " + str(n) + ": "
-        for f in range(index_y):
-            if cast_to_int:
-                print_string += str(int(var[(f, n)].varValue)) + " "
-            else:
-                print_string += "{0:.5f} ".format(round(var[(f, n)].varValue, 5))
-        print(print_string)
-    print(" ")
-
-
-def print_location_adaptive(var, index_x, indeces, cast_to_int=True):
-    header = " " * 7
-    if cast_to_int:
-        space = " "
-    else:
-        space = " " * 7
-    for index in indeces:
-        header += space + str(index)
-    print(header)
-    for n in range(index_x):
-        print_string = "Node " + str(n) + ": "
-        for f in indeces:
-            if cast_to_int:
-                print_string += str(int(var[(f, n)].varValue)) + " "
-            else:
-                print_string += "{0:.5f} ".format(round(var[(f, n)].varValue, 5))
-        print(print_string)
-    print(" ")
+from utils import print_location, print_location_adaptive
 
 
 def solve_split_adaptive(param_fragment_sizes, param_query_compositions, param_query_frequencies,
@@ -158,116 +116,6 @@ def solve_split_adaptive(param_fragment_sizes, param_query_compositions, param_q
     return problem, var_location, var_runnable, var_workshare, space_required
 
 
-def solve_split(param_fragment_size, param_queries, param_query_frequency, param_query_cost,
-                param_num_nodes):
-    def generate_index_dict(first_index, second_index):
-        dict_index = []
-        for i in range(first_index):
-            for j in range(second_index):
-                dict_index.append((i, j))
-        return dict_index
-
-    def objective():
-        sum = 0
-        for f in range(param_num_fragments):
-            for n in range(param_num_nodes):
-                sum += var_location[(f, n)] * param_fragment_size[f]
-        sum += 1000 * var_epsilon
-        return sum
-
-    def nb_1(problem_instance):
-        for q in range(param_num_queries):
-            c = sum([var_runnable[(q, n)] for n in range(param_num_nodes)]) >= 1
-            problem_instance += c
-        return problem_instance
-
-    def nb_2(problem_instance):
-        for q in range(param_num_queries):
-            c = sum([var_workshare[(q, n)] for n in range(param_num_nodes)]) >= 1
-            problem_instance += c
-        return problem_instance
-
-    def nb_3(problem_instance):
-        for n in range(param_num_nodes):
-            for q in range(param_num_queries):
-                c = var_runnable[(q, n)] * sum(
-                    [param_queries[q][f] for f in range(param_num_fragments)]) <= sum(
-                    [var_location[(f, n)] * param_queries[q][f] for f in
-                     range(param_num_fragments)])
-                problem_instance += c
-        return problem_instance
-
-    def nb_4(problem_instance):
-        for n in range(param_num_nodes):
-            for q in range(param_num_queries):
-                c = var_workshare[(q, n)] <= var_runnable[q, n]
-                problem_instance += c
-        return problem_instance
-
-    def nb_5(problem_instance):
-        for n in range(param_num_nodes):
-            for w in range(len(param_query_workload)):
-                c = (sum([var_workshare[(q, n)] * param_query_workload[w][q] for q in
-                          range(param_num_queries)]) / param_total_workload[w]) <= var_epsilon
-                problem_instance += c
-        return problem_instance
-
-    problem = LpProblem("replication", LpMinimize)
-
-    param_num_fragments = len(param_fragment_size)
-    param_num_queries = len(param_query_cost)
-
-    param_query_workload = [[a * b for a, b in zip(param_query_frequency[i], param_query_cost)] for
-                            i in range(len(param_query_frequency))]
-    param_total_workload = [sum(param_query_workload[i]) for i in range(len(param_query_workload))]
-
-    location_dict_index = generate_index_dict(param_num_fragments, param_num_nodes)
-    runnable_dict_index = generate_index_dict(param_num_queries, param_num_nodes)
-    workshare_dict_index = generate_index_dict(param_num_queries, param_num_nodes)
-
-    var_location = LpVariable.dicts(name="location", indexs=location_dict_index, lowBound=0,
-                                    upBound=1, cat='Integer')
-    var_runnable = LpVariable.dicts(name="runnable", indexs=runnable_dict_index, lowBound=0,
-                                    upBound=1, cat='Integer')
-    var_workshare = LpVariable.dicts(name="workshare", indexs=workshare_dict_index, lowBound=0,
-                                     cat='Continuous')
-    var_epsilon = LpVariable(name="epsilon", lowBound=0, cat='Continuous')
-
-    problem += objective()
-    problem = nb_1(problem)
-    problem = nb_2(problem)
-    problem = nb_3(problem)
-    problem = nb_4(problem)
-    problem = nb_5(problem)
-
-    problem.solve()
-
-    print("")
-    print("##### LOCATION #####")
-    print_location(var_location, param_num_nodes, param_num_fragments)
-
-    print("")
-    print("##### RUNNABLE #####")
-    print_location(var_runnable, param_num_nodes, param_num_queries)
-
-    print("")
-    print("##### WORKSHARE #####")
-    print_location(var_workshare, param_num_nodes, param_num_queries, False)
-
-    print("")
-
-    sum_workload = 0
-    for loc in var_workshare.keys():
-        sum_workload += var_workshare[loc].varValue
-
-    print("Sum Workload: ", str(sum_workload))
-    print("Objective Value:", str(problem.objective.value() - 1000 * var_epsilon.value()))
-    print("Epsilon:", str(var_epsilon.value()), "(Optimum ",
-          "{})".format(str(float(1) / param_num_nodes)))
-    print('Nr. Vars:', problem.numVariables())
-    return problem, var_location, var_runnable, var_workshare
-
-
 class Problem:
     def __init__(self, param_fragment_size, param_queries, param_query_frequency,
                  param_query_cost, param_query_ids, total_nr_queries):
@@ -312,16 +160,7 @@ class Node(AbstractNode):
 
 def main():
     param_num_nodes = 4
-    split1 = Node("split1")
-    split2 = Node("split2", parent=split1)
-    split3 = Node("split3", parent=split1)
-    split4 = Node("split4", parent=split2)
-    split5 = Node("split5", parent=split2)
-    split6 = Node("split4", parent=split2)
-    split7 = Node("split6", parent=split3)
-    split8 = Node("split7", parent=split3)
 
-    print(RenderTree(split1, style=DoubleStyle))
 
     param_fragment_size = [1, 2, 3, 4, 4, 1, 2]
     param_queries = [[1, 1, 0, 1, 1, 1, 0],
@@ -341,23 +180,75 @@ def main():
     assert len(param_query_frequency[0]) == len(param_query_cost) \
            == len(param_query_ids) == len(param_queries)
 
+    split1 = tree1()
+    print(RenderTree(split1, style=DoubleStyle))
     split1.problem = Problem(param_fragment_size, param_queries,
                              param_query_frequency, param_query_cost, param_query_ids,
                              len(param_query_ids))
-    total_space = [node.solve() for node in PreOrderIter(split1)]
+
+    #total_space = [node.solve() for node in PreOrderIter(split1)]
+    #print('Split Space required', total_space)
+
+
+    root2 = tree2()
+    print(RenderTree(root2, style=DoubleStyle))
+    root2.problem = Problem(param_fragment_size, param_queries,
+                             param_query_frequency, param_query_cost, param_query_ids,
+                             len(param_query_ids))
+
+
+    total_space = [node.solve() for node in PreOrderIter(root2)]
     print('Split Space required', total_space)
     # The leave nodes present no problem and are not solved, thus the tree that is defined here
     # has only three splits !!!!!
 
-    problem = solve_split(param_fragment_size, param_queries, param_query_frequency,
-                          param_query_cost, param_num_nodes)
     #
-    # problem = solve_split_adaptive(param_fragment_size, param_queries, param_query_frequency,
-    #                       param_query_cost, param_num_nodes,[0,1,2,3,4,5,6])
+    problem = solve_split_adaptive(param_fragment_size, param_queries, param_query_frequency,
+                          param_query_cost, param_num_nodes,param_query_ids, 'complete')
 
     print('Minimum possible would be:', sum(param_fragment_size))
     print('Workload per queriy: ', [[a * b for a, b in zip(param_query_frequency[i], param_query_cost)] for
                             i in range(len(param_query_frequency))])
+
+def tree1():
+    "Binary"
+    split1 = Node("split1")
+    split2 = Node("split2", parent=split1)
+    split3 = Node("split3", parent=split1)
+    split4 = Node("split4", parent=split2)
+    split5 = Node("split5", parent=split2)
+    split6 = Node("split6", parent=split3)
+    split7 = Node("split7", parent=split3)
+    Node("l1", parent=split4)
+    Node("l2", parent=split4)
+    Node("l3", parent=split5)
+    Node("l4", parent=split5)
+    Node("l5", parent=split6)
+    Node("l6", parent=split6)
+    Node("l7", parent=split7)
+    Node("l8", parent=split7)
+
+    return split1
+
+
+def tree2():
+    "four split"
+    root = Node("split1")
+    split2 = Node("split2", parent=root)
+    split3 = Node("split3", parent=root)
+    split4 = Node("split4", parent=root)
+    split5 = Node("split5", parent=root)
+    Node("l5", parent=split2)
+    Node("l6", parent=split2)
+    Node("l7", parent=split3)
+    Node("l8", parent=split3)
+    Node("l1", parent=split4)
+    Node("l2", parent=split4)
+    Node("l3", parent=split5)
+    Node("l4", parent=split5)
+
+    return root
+
 
 
 if __name__ == '__main__':
