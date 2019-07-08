e6f0f356c09346d4f16df22be1dde0c6c76e4a05
Marvin Thiele <MarvinThiele@gmx.net>
2019-07-08 18:23:49 +0200

    First Approach to Automatic Evaluation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from anytree import RenderTree, DoubleStyle, LevelOrderIter
 
@@ -18,7 +19,7 @@ class Problem:
 
 
 def main():
-    param_num_nodes = 16
+    param_num_nodes = 8
 
     param_fragment_size = [1, 2, 3, 4, 4, 1, 2]
     param_queries = [[1, 1, 0, 1, 1, 1, 0],
@@ -45,16 +46,16 @@ def main():
                       param_query_frequency, param_query_cost, param_query_ids,
                       len(param_query_ids))
 
-    s1 = solve_for_tree(prime_factor_tree(param_num_nodes), problem)
+    #s1 = solve_for_tree(prime_factor_tree(param_num_nodes), problem)
     #s2 = solve_for_tree(prime_factor_tree(param_num_nodes, True), problem)
     #s3 = solve_for_tree(prime_factor_tree(param_num_nodes, False, True), problem)
-    #s4 = solve_for_tree(prime_factor_tree(param_num_nodes, True, True), problem)
+    s4 = solve_for_tree(prime_factor_tree(param_num_nodes, True, True), problem)
     #s5 = solve_for_tree(one_split_tree(param_num_nodes),problem, 2)
     #s6 = solve_for_tree(one_vs_all_split(param_num_nodes), problem, 2)
-    s7 = solve_for_tree(approximate_tree(param_num_nodes, 5), problem)
+
     #print(s1)#, s2, s3,s4, s5)
-    print(s1)
-    print(s7)
+    #print(s4)
+    automated_test(5)
 
     print('Minimum possible would be:', sum(param_fragment_size))
     print('Workload per query: ',
@@ -78,7 +79,47 @@ def solve_for_tree(tree_root, problem, timeout=None):
     aborted = runtime >= timeout if timeout else False
     return Observation(sum(total_space), end - start, tree_root, aborted)
 
+def generate_queries(num_queries, num_fragments):
+    queries = []
+    for i in range(num_queries):
+        temp_list = []
+        for j in range(num_fragments):
+            temp_list.append(random.choice([0, 1]))
+        queries.append(temp_list)
+    return queries
 
+def automated_test(num_epochs):
+    total_results = []
+    for node_count in range(2,5):
+        epoch_results = []
+        for epoch in range(num_epochs):
+            param_num_fragments = 7
+            param_num_queries = 7
+
+            param_fragment_size = random.sample(range(1, 100), param_num_fragments)
+            param_queries = generate_queries(param_num_queries, param_num_fragments)
+            param_query_frequency = [random.sample(range(1, 100), param_num_queries) for i in range(3)]
+            param_query_cost = random.sample(range(1, 100), param_num_queries)
+            param_query_ids = [0, 1, 2, 3, 4, 5, 6]
+
+            problem = Problem(param_fragment_size, param_queries,
+                              param_query_frequency, param_query_cost, param_query_ids,
+                              len(param_query_ids))
+
+            #s1 = solve_for_tree(prime_factor_tree(node_count, True, True), problem)
+            s2 = solve_for_tree(one_split_tree(node_count), problem, 2)
+            s3 = solve_for_tree(one_vs_all_split(node_count), problem, 2)
+
+            epoch_results.append([])
+            #epoch_results[epoch].append(s1)
+            epoch_results[epoch].append(s2)
+            epoch_results[epoch].append(s3)
+        total_results.append(epoch_results)
+
+    for node_level in total_results:
+        sum_s1 = sum([node_level[i][0].space for i in range(len(node_level))]) / num_epochs
+        sum_s2 = sum([node_level[i][1].space for i in range(len(node_level))]) / num_epochs
+        print(sum_s1, sum_s2)
 if __name__ == '__main__':
     main()
 
