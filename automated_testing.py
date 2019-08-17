882cf1fbafe71b0cbdcbbbef183d1062520d2b32
Anton von Weltzien <a.weltzien@outlook.de>
2019-08-17 13:46:33 +0200

    Update fragment to query assignment

--- automated_testing.py
+++ automated_testing.py
@@ -18,15 +18,24 @@ mpl.rcParams['figure.dpi'] = 400
 
 def generate_queries(num_queries, num_fragments):
     queries = []
-    for i in range(num_queries):
+    used = [0] * num_fragments
+    for q in range(num_queries - 1):
         temp_list = []
-        for j in range(num_fragments):
-            temp_list.append(random.choice([0, 1]))
+        for f in range(num_fragments):
+            use = random.choice([0, 1])
+            used[f] = 1 if use else used[f]
+            temp_list.append(use)
+        if sum(temp_list) == 0:
+            temp_list[random.choice(range(num_fragments))] = 1  # to avoid empty query
         queries.append(temp_list)
+    used[0] = 0  # to avoid empty query
+    queries.append([0 if i else 1 for i in used])
     return queries
 
 
 def automated_test():
+    #df = test_2_2()
+    #exit()
     # What do you want to test?
     test_node_count = False
     test_pareto = False
@@ -58,8 +67,8 @@ def automated_test():
     TIMEOUT_should_squeeze = False
     TIMEOUT_epsilon_factor = 10000
 
-
-    problems = generate_problems(num_problems, min_fragments, max_fragments, min_queries, max_queries, num_workloads)
+    problems = generate_problems(num_problems, min_fragments, max_fragments, min_queries,
+                                 max_queries, num_workloads)
     df = None
 
     if test_node_count:
@@ -89,7 +98,37 @@ def automated_test():
 
 
 def test_2_2():
-    problem1 = Problem()
+    sizes = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    nodes = sizes
+    fragments = sizes
+    queries = sizes
+    df = pd.DataFrame(columns=['nodes', 'algo', 'time', 'query', 'fragment', 'space', 'deviation'])
+    df.nodes = df.nodes.astype('int')
+    df.algo = df.algo.astype('str')
+    df.fragment = df.time.astype('int')
+    df.space = df.space.astype('int')
+    df.query = df.deviation.astype('int')
+    for fragment in fragments:
+        for query in queries:
+            problem = add_problem_properties(fragment, query, 1)
+            for node in nodes:
+                s = solve_for_tree(one_split_tree(node), problem, 900, False, 0)
+                df.loc[len(df)] = [node, s.name.split('|')[0], s.time, query, fragment, s.space,
+                                   s.deviation]
+    df.to_csv('test22', index=False)
+    n = df.groupby('nodes', as_index=False).mean()
+    q = df.groupby('query', as_index=False).mean()
+    f = df.groupby('fragment', as_index=False).mean()
+    fig, ax = plt.subplots()
+    n.plot(y='time', x='nodes', ax=ax, label='#nodes')
+    q.plot(y='time', x='query', ax=ax, label='#queries')
+    f.plot(y='time', x='fragment', ax=ax, label='#fragments')
+    ax.set(xlabel='', ylabel='average time in s',
+           title='average runtime for factors')
+    plt.xticks(sizes)
+    plt.show()
+    return df
+
 
 def test_with_nodes(problems, min_nodes, max_nodes, timeout, should_squeeze, epsilon_factor):
     total_results = []
@@ -98,7 +137,8 @@ def test_with_nodes(problems, min_nodes, max_nodes, timeout, should_squeeze, eps
     for node_count in range(min_nodes, max_nodes + 1):
         epoch_results = []
         for problem in problems:
-            print(f"\n SOLVING: NODE COUNT { node_count }, PROBLEM { problems.index(problem) } AT { str(datetime.now()) }")
+            print(
+                f"\n SOLVING: NODE COUNT {node_count}, PROBLEM {problems.index(problem)} AT {str(datetime.now())}")
             # s1 = solve_for_tree(one_split_tree(node_count), problem, timeout)
             sys.stdout = open(os.devnull, "w")
             s11 = solve_for_tree(one_split_tree(node_count), problem, timeout, should_squeeze,
@@ -153,7 +193,8 @@ def epsilon_pareto_front(problems, selected_node_count, timeout, epsilon_factor_
         epoch_results = []
         for problem in problems:
             # s1 = solve_for_tree(one_split_tree(node_count), problem, timeout)
-            print(f"\n SOLVING: EPSILON FAC {epsilon_factor}, PROBLEM {problems.index(problem)} AT {str(datetime.now())}")
+            print(
+                f"\n SOLVING: EPSILON FAC {epsilon_factor}, PROBLEM {problems.index(problem)} AT {str(datetime.now())}")
             s11 = solve_for_tree(one_split_tree(selected_node_count), problem, timeout, True,
                                  epsilon_factor)
 
@@ -197,6 +238,7 @@ def epsilon_pareto_front(problems, selected_node_count, timeout, epsilon_factor_
     df.deviation = df.deviation.astype('float')
     return total_results, df
 
+
 def timeout_tests(problems, selected_node_count, maximum_timeout, should_squeeze, epsilon_factor):
     total_results = []
     df = pd.DataFrame(columns=['timeout', 'algo', 'time', 'space', 'deviation'])
@@ -205,13 +247,16 @@ def timeout_tests(problems, selected_node_count, maximum_timeout, should_squeeze
     for timeout in timout_array:
         epoch_results = []
         for problem in problems:
-            print(f"\n SOLVING: TIMEOUT {timeout}, PROBLEM {problems.index(problem)} AT {str(datetime.now())}")
+            print(
+                f"\n SOLVING: TIMEOUT {timeout}, PROBLEM {problems.index(problem)} AT {str(datetime.now())}")
             # s1 = solve_for_tree(one_split_tree(node_count), problem, timeout)
-            s1 = solve_for_tree(one_split_tree(selected_node_count), problem, timeout, should_squeeze, epsilon_factor)
-            s2 = solve_for_tree(one_split_tree(selected_node_count), problem, timeout, True, epsilon_factor)
+            s1 = solve_for_tree(one_split_tree(selected_node_count), problem, timeout,
+                                should_squeeze, epsilon_factor)
+            s2 = solve_for_tree(one_split_tree(selected_node_count), problem, timeout, True,
+                                epsilon_factor)
             s1.name = "OneSplitNoEP"
             s2.name = "OneSplitWithEP"
-            xx_results = [s1,s2]
+            xx_results = [s1, s2]
             for res in xx_results:
                 # The name is split due to the very verbose and varying naming for prime trees
                 df.loc[len(df)] = [timeout, res.name.split('|')[0], res.time, res.space,
@@ -231,7 +276,8 @@ def timeout_tests(problems, selected_node_count, maximum_timeout, should_squeeze
     return total_results, df
 
 
-def generate_problems(num_epochs, min_fragments, max_fragments, min_queries, max_queries, num_workloads):
+def generate_problems(num_epochs, min_fragments, max_fragments, min_queries, max_queries,
+                      num_workloads):
     problems = []
     for epoch in range(num_epochs):
         param_num_fragments = random.sample(range(min_fragments, max_fragments), 1)[0]
@@ -275,10 +321,11 @@ def plot_data(df, min_nodes, max_nodes):
            title='%-Deviation from optimum One Split Strategy')
     plt.show()
 
+
 def plot_data_pareto(df):
     plot_group = df.groupby(['algo', 'epsilon'], as_index=False).mean()
     for algo in plot_group['algo'].unique():
-        fig, axs = plt.subplots(1, 3, figsize=(10,3))
+        fig, axs = plt.subplots(1, 3, figsize=(10, 3))
         axs[0].set(xlabel='Space', ylabel='Deviation', title=f'Space / Deviation for {algo}')
         color = plot_group[plot_group.algo == algo]['epsilon'].apply(lambda x: math.log(x, 10))
         plot_group[plot_group.algo == algo].plot.scatter(x='space',
@@ -301,10 +348,11 @@ def plot_data_pareto(df):
         plt.tight_layout()
         plt.show()
 
+
 def plot_data_timeout(df):
     plot_group = df.groupby(['algo', 'timeout'], as_index=False).mean()
     for algo in plot_group['algo'].unique():
-        fig, axs = plt.subplots(1, 3, figsize=(10,3))
+        fig, axs = plt.subplots(1, 3, figsize=(10, 3))
         axs[0].set(xlabel='Timeout', ylabel='Space', title='Space / TImeout Relation')
         plot_group[plot_group.algo == algo].plot.scatter(x='timeout',
                                                          y='space',
